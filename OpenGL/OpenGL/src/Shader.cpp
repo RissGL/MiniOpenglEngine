cb5bd52605e6e18a5a725d6289992164224f5cdf
RissGL <zhangdeyan792@gmail.com>
2026-04-07 18:44:17 +0800

    3D

--- OpenGL/OpenGL/src/Shader.cpp
+++ OpenGL/OpenGL/src/Shader.cpp
@@ -51,7 +51,7 @@ void Shader::SetUniformMat4f(const std::string& name,const glm::mat4& matrix)
     GLCALL(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE,&matrix[0][0]));
 }
 
-int Shader::GetUniformLocation(const std::string& name) 
+int Shader::GetUniformLocation(const std::string& name) const
 {
     if (m_UniformLocationCaChe.find(name) != m_UniformLocationCaChe.end())
         return m_UniformLocationCaChe[name];
